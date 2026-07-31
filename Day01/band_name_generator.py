928f4be49c8ddede76e3c93dead792b786aa63cc
Aniket Patil <aniketpatil8149@gmail.com>
2026-07-31 15:04:51 +0530

    Initial commit - Day 1 Band Name Generator

---
+++ Day01/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("Which city do you like?\n")
+pet = input("Which pet is your favourite ?\n")
+print("Your Band name could be "+city+" "+pet)
